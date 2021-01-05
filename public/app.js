be93fb6b6b129a32a7bffaf036db8a3d67670256
Daniel <rodriguez.flores.daniel01@gmail.com>
2021-01-05 01:23:09 +0000

    Pinturillo con Node JS

---
+++ public/app.js
@@ -0,0 +1,231 @@
+var socket = io();
+var usuario;
+
+//Funcion para preguntar el username
+function preguntarUsername(){
+
+    $('.grey-out').fadeIn(500);
+    $('.usuario').fadeIn(500);
+    $('.usuario').submit(function(){
+        event.preventDefault();
+        usuario = $('#username').val().trim();
+
+        //Si el usuario esta vacio
+        if (usuario == '') {
+            return false
+        };
+
+        var index = usuarios.indexOf(usuario);
+
+        if (index > -1) {
+            alert(usuario + ' ya existe');
+            return false
+        };
+        
+        //Unimos al usuario
+        socket.emit('join', usuario);
+        $('.grey-out').fadeOut(300);
+        $('.usuario').fadeOut(300);
+        $('input.adivinar-input').focus();
+    });
+
+};
+
+var context;
+var canvas;
+var click = false;
+
+var limpiarPantalla = function(){
+
+    //Limpiamos la pantalla dandole los valores x y
+    context.clearRect(0, 0, canvas[0].width, canvas[0].height);
+
+};
+
+
+//Decimos a que es igual el concursante
+var adivinador = function(){
+
+    limpiarPantalla();
+    click = false;
+    console.log('Estatus del dibujo ' + click);
+    $('.dibujar').hide();
+    $('#adivinadores').empty();
+    console.log('Eres adivinador');
+    $('#adivinar').show();
+    $('.adivinar-input').focus();
+
+    //Mosramos la palabra a adivinar
+    $('#adivinar').on('submit', function() {
+        event.preventDefault();
+        var adivinar = $('.adivinar-input').val();
+
+        if (adivinar == '') {
+            return false
+        };
+
+        console.log(usuario + " dijo que es: " + adivinar);
+        socket.emit('adivinarpalabra', {username: usuario, adivinarpalabra: adivinar});
+        $('.adivinar-input').val('');
+    });
+
+};
+
+var adivinarpalabra = function(data){
+
+    $('#adivinadores').append('<li>' +data.username + " dijo que es: " + data.adivinarpalabra+ '</li>').scrollTop($("#adivinadores").height());
+
+    if (click == true && data.adivinarpalabra == $('span.word').text() ) {
+        console.log('adivinador: ' + data.username + ' draw-word: ' + $('span.word').text());
+        socket.emit('respuesta correcta', {username: data.username, adivinarpalabra: data.adivinarpalabra});
+        socket.emit('swap rooms', {from: usuario, to: data.username});
+        click = false;
+    }
+
+};
+
+var dibujarPalabra = function(word){
+
+    $('span.word').text(word);
+    console.log('Tienes que dibujar: ' + word);
+
+};
+
+var usuarios = [];
+
+var listausuarios = function(names){
+
+    usuarios = names;
+
+    //Insertamos codigo html
+    var html = '<p class="chatbox-header">' + 'Jugadores' + '</p>';
+    
+    //Mostramos la Lista de usuarios en el html
+    for (var i = 0; i < names.length; i++) {
+        html += '<li>' + names[i] + '</li>';
+    };
+    $('ul').html(html);
+
+};
+
+//Creamos un nuevo dibujante
+var nuevoDibujante = function(){
+    
+    socket.emit('nuevo dibujante', usuario);
+    limpiarPantalla();
+    $('#adivinadores').empty();
+
+};
+
+var respuestaCorrecta = function(data){
+    //Mostramos un mensaje en html
+    $('#adivinadores').html('<p>' + data.username + ' adivino la palabra!' + '</p>');
+};
+
+var reset = function(name) {
+    limpiarPantalla();
+    $('#adivinadores').empty();
+    console.log('Nuevo dibujante: ' + name);
+    //Mostramos quien es el nuevo dibujante
+    $('#adivinadores').html('<p>' + name + ' es el nuevo Dibujante' + '</p>');
+};
+
+var dibujar = function(obj){
+
+    //Obtenemos el color con el que dibuje
+    context.fillStyle = obj.color;
+    context.beginPath();
+    context.arc(obj.position.x, obj.position.y,
+         3, 0, 2 * Math.PI);
+    
+    context.fill();
+
+};
+
+var pictionary = function(){
+
+    limpiarPantalla();
+    click = true;
+    console.log('Estatus del dibujo: ' + click);
+    $('#adivinar').hide();
+    $('#adivinadores').empty();
+    $('.dibujar').show();
+
+    var dibujando;
+    var color;
+    var obj = {};
+
+    $('.draw-buttons').on('click', 'button', function(){
+        obj.color = $(this).attr('value');
+        console.log(obj.color);
+
+        if (obj.color === '0') {
+            socket.emit('limpiar pantalla', usuario);
+            //El color por defecto sera el blanco
+            context.fillStyle = 'white';
+            return;
+        };
+    });
+
+    console.log('Eres el dibujante');
+
+    $('.usuarios').on('dblclick', 'li', function() {
+        //Si dan doble click intercambiamos los roles
+        if (click == true) {
+            var target = $(this).text();
+            socket.emit('swap rooms', {from: usuario, to: target});
+        };
+    });
+
+    //--------------------Eventos de movimiento del mouse-------------------
+
+    //Movimiento hacia abajo
+    canvas.on('mousedown', function(event) { 
+        dibujando = true;   
+    });
+
+    //Movimiento hacia arriba
+    canvas.on('mouseup', function(event) {
+        dibujando = false;
+    });
+
+    //Movimiento del mouse
+    canvas.on('mousemove', function(event) {
+        var offset = canvas.offset();
+
+        //Posicion del dibujo
+        obj.position = {x: event.pageX - offset.left,
+                        y: event.pageY - offset.top};
+        
+        if (dibujando == true && click == true) {
+            dibujar(obj);
+            socket.emit('dibujar', obj);
+        };
+    });
+
+};
+
+
+//Parametros a usar en el juego
+$(document).ready(function() {
+
+    canvas = $('#canvas');
+    context = canvas[0].getContext('2d');
+    canvas[0].width = canvas[0].offsetWidth;
+    canvas[0].height = canvas[0].offsetHeight;
+
+    preguntarUsername();
+
+    socket.on('listausuarios', listausuarios);
+    socket.on('adivinador', adivinador);
+    socket.on('adivinarpalabra', adivinarpalabra);
+    socket.on('dibujar', dibujar);
+    socket.on('dibujar palabra', dibujarPalabra);
+    socket.on('dibujante', pictionary);
+    socket.on('nuevo dibujante', nuevoDibujante);
+    socket.on('respuesta correcta', respuestaCorrecta);
+    socket.on('reset', reset);
+    socket.on('limpiar pantalla', limpiarPantalla);
+
+});
+
---
+++ server.js
@@ -0,0 +1,190 @@
+//Variables que necesitaremos
+
+var http = require('http');
+var express = require('express');
+var socket_io = require('socket.io');
+
+var app = express();
+app.use(express.static('public'));
+
+var server = http.Server(app);
+var io = socket_io(server);
+
+//Aqui guardaremos los usuarios
+var usuarios = [];
+
+//Guardamos las palabras que apareceran en el juego
+var palabras = ["disfraz", "chatarra", "demoler", "instrucciones",
+                "descubrimiento","alfombra","taza", "navidad",
+                "oveja", "rata", "ballena", "perro", "gato",
+                "avion", "tirar", "redondo", "bandera",
+                "pavo", "turbulencia", "comer", "pelear",
+                "escribir", "isla", "juventud", "enseñar",
+                "niño", "hilo", "festejar", "mar", "armario",
+                "militar", "extraterrestre", "juegos", "caballo",
+                "ventilador", "aniversario", "disco", "estanque",
+                "maleta", "lombriz", "rata", "estufa", "auto",
+                "diamante", "radio", "tormenta", "medianoche",
+                "moneda", "sombra", "relatar", "arrastrarse", "fama"];
+
+//Creamos la funcion para generar una nueva palabra
+function nuevaPalabra() {
+
+    numpalabra = Math.floor(Math.random() * (palabras.length));
+    return palabras[numpalabra];
+
+};
+
+var numpalabra;
+
+
+//Funcion para conectarse al socket
+io.on('connection', function (socket){
+
+    io.emit('listausuarios', usuarios);
+
+    //Funcion para unirse a la sala
+    socket.on('join', function (name){
+
+        socket.username = name;
+
+        //Unimos al usuario a una sala
+        socket.join(name);
+        console.log(socket.username + " se unio a la partida. Id: " + socket.id);
+
+        //Guardamos el nombre del usuario en el array usuarios
+        usuarios.push(socket.username);
+
+        //Creamos la condicion para el primer dibujante (primero en unirse)
+        if (usuarios.length == 1 || typeof io.sockets.adapter.rooms['dibujante'] === 'undefined'){
+
+            socket.join('dibujante');
+            //convertimos al usuario en dibujante
+            io.in(socket.username).emit('dibujante', socket.username);
+            console.log(socket.username + " es el dibujante");
+
+            //Enviamos una palabra al azar al dibujante
+            io.in(socket.username).emit('dibujar palabra', nuevaPalabra());
+
+        }
+        //Si hay alguien mas en la sala
+        
+        else{
+            //si se unen mas personas seran concursantes
+            socket.join('adivinador');
+
+            // Enviamos el evento para convertir en adivinador a este usuario
+            io.in(socket.username).emit('adivinador', socket.username);
+            console.log(socket.username + 'es un adivinador');
+
+        }
+
+        io.emit('listausuarios', usuarios);
+
+    });
+
+    //Mostramos el dibujo
+    socket.on('dibujar', function(obj){
+
+        socket.broadcast.emit('dibujar', obj);
+
+    });
+
+    //Mostramos las palabras que envian los usuarios
+    socket.on('adivinarpalabra', function(data){
+
+        io.emit('adivinarpalabra', {username: data.username, adivinarpalabra: data.adivinarpalabra});
+        console.log(data.username + 'dice que es: ' + data.adivinarpalabra);
+
+
+    });
+
+    //Realizamos la funcion para cuando se desconecte el usuario
+    socket.on('disconnect', function(){
+
+        for(var i = 0; i < usuarios.length; i++){
+
+            //Eliminamos al usuario de la lista
+            if(usuarios[i] == socket.username){
+                usuarios.splice(i, 1);
+            };
+        };
+        console.log(socket.username + ' se ha desconectado');
+
+        //Actualizamos la lista de usarios
+        io.emit('listausuarios', usuarios);
+
+        //Si el dibujante no tiene conexion o no existe
+        if( typeof io.sockets.adapter.rooms['dibujante'] === "undefined"){
+
+            //Generamos un numero al azar para escoger el nuevo dibujante
+            var x = Math.floor(Math.random() * (usuarios.length));
+            console.log(usuarios[x]);
+
+            //Asignamos a un nuevo dibujante
+            io.in(usuarios[x]).emit('nuevo dibujante', usuarios[x]);
+
+        };
+
+    });
+
+    //Funcion para el nuevo dibujante
+    socket.on('nuevo dibujante', function(name){
+
+        socket.leave('adivinador');
+
+        //Agregamos a un nuevo dibujante
+        socket.join('dibujante');
+        console.log('El nuevo dibujante es: ' + name);
+
+        socket.emit('dibujante', name);
+
+        //Enviamos la palabra a dibujar
+        io.in('dibujante').emit('dibujar palabra', nuevaPalabra());
+
+    });
+
+
+    //Funcion para intercambio de roles
+    socket.on('swap rooms', function(data){
+
+        //El dibujante pasa a la sala de concursante
+        socket.leave('dibujante');
+        socket.join('adivinador');
+        socket.emit('adivinador', socket.username);
+
+        //Convertimos en dibujante a otro usuario
+        io.in(data.to).emit('dibujante', data.to);
+
+        //Generamos nueva palabra
+        io.in(data.to).emit('dibujar palabra', nuevaPalabra());
+        io.emit('reset', data.to);
+
+    });
+
+    //Funcion para respuesta correcta
+    socket.on('respuesta correcta', function(data){
+
+        io.emit('respuesta correcta', data);
+        console.log(data.username + 'adivino la palabra ' + data.adivinar);
+
+    });
+
+    //Funcion para limpiar pantalla
+    socket.on('limpiar pantalla', function(name){
+
+        io.emit('limpiar pantalla', name);
+
+    });
+
+});
+
+
+//Servidorrr
+
+server.maxConnections = 6;
+server.listen(process.env.PORT || 8080, function(){
+
+    console.log('Servidor escuchando en el puerto 8080');
+
+});
